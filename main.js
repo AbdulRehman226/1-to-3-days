3ea44d79194548c876064b16fab74d1d74878fc3
AbdulRehman226 <163383150+AbdulRehman226@users.noreply.github.com>
2024-03-14 11:05:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// *Question 2:* Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Abdul Rehman, would you like tolearn some TypeScript today?
+export {};
+// let name:string ="Abdul Rehman";
+// console.log(`Hello ${name},would you like learn some typescript today?`);
+//  *Question 3:* Name Cases: Store a person’s name in a variable, and then print  that person’s name in lowercase, uppercase, and titlecase.
+// let name: string = "Abdul Rehman";
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+// console.log(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
+// *Question 4:* Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+// console.log('Adolf Hitler once said,"If you want to shine like a sun you have to burn like it".');
+// *Question 5:* Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// let famous_person: string ="Adolf Hitler";
+// let message: string = `${famous_person} once said "If you want to shine like a sun burn like it".`;
+// console.log (message)
+// *Question 6:* Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+// let name:string = "\t\n Abdul Rehman \t\n";
+// console.log(name);
+// console.log(name.trim()); 
+// *Question 7:* Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+// console.log(5+3); //Addition
+// console.log(12-4); //Subtraction
+// console.log(4*2); //Multiplication
+// console.log(32/4); //Division
+// *Question 8:* You should create four lines that look like this:console.log(5 + 3)Your output should simply be four lines with the number 8 appearing once on each line.
+//8
+// console.log(5+3);
+// console.log(16-8);
+// console.log(8*1);
+// console.log(24/3);
+// *Question 9:* Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+// let favorsitenumber: number = 10;
+// console.log(`My Favorite Number is ${favorsitenumber}.`);
